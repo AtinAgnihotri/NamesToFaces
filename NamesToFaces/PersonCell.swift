835ffef0093dffcc7f43ffe6050333cf07a6776b
AtinAgnihotri <atinagnihotri@gmail.com>
2021-07-22 00:23:26 +0530

    Adds PersonCell class

---
+++ NamesToFaces/PersonCell.swift
@@ -0,0 +1,15 @@
+//
+//  PersonCell.swift
+//  NamesToFaces
+//
+//  Created by Atin Agnihotri on 22/07/21.
+//
+
+import UIKit
+
+class PersonCell: UICollectionViewCell {
+    
+    @IBOutlet var personImage: UIImageView!
+    @IBOutlet var personName: UILabel!
+    
+}
